300e752027d94e3406ef704ec80be83ac42bfb45
Rodrigo Santiago <rodrigo.santiago@murah.com.br>
2022-05-26 01:07:10 -0300

    doc

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,6 +1,6 @@
 /opt/maven/apache-maven-3.6.3/bin/mvn clean package
 /opt/jdk-11.0.9/bin/java -jar target/minesweeper-1.0.1-SNAPSHOT.jar
-/opt/jdk-11.0.9/bin/java -jar target/quarkus-app/quarkus-run.jar
+# /opt/jdk-11.0.9/bin/java -jar target/quarkus-app/quarkus-run.jar
 
 sudo chown -R $USER pgdata
 docker build -f src/main/docker/Dockerfile.jvm -t rodrigosntg/minesweeper .
